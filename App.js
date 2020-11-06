7add5ef4162c9585ee4c7289b86fdc68347ee2e9
wadereynes <reynes.wm@sprobe.com>
2020-11-07 00:14:26 +0800

    087

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 // import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState } from 'react'
+import { View, TextInput, Text, Switch } from 'react-native'
 
-import Card from './app/components/Card'
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen'
+import Screen from './app/components/Screen'
+import Icon from './app/components/Icon'
+import ListItem from './app/components/ListItem'
+import AccountScreen from './app/screens/AccountScreen'
+import ListingsScreen from './app/screens/ListingsScreen'
+import AppTextInput from './app/components/AppTextInput'
+import AppPicker from './app/components/AppPicker'
+import LoginScreen from './app/screens/LoginScreen'
+import ListingEditScreen from './app/screens/ListingEditScreen'
+
+// import Card from './app/components/Card'
+// import ListingDetailsScreen from './app/screens/ListingDetailsScreen'
+// import ViewImageScreen from './app/screens/ViewImageScreen'
+// import MessagesScreen from './app/screens/MessagesScreen'
+
 export default function App() {
-  return <ListingDetailsScreen />
+  return <ListingEditScreen />
 }
